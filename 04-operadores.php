87a605d548cbf4e64de1fb9e0e006f60632d5f29
oscar cervantes <oscarcr189@outlook.com>
2023-03-25 23:45:22 -0600

    actualizacion1

--- 04-operadores.php
+++ 04-operadores.php
@@ -1,6 +1,18 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 10;
+//sumar
+echo $numero1 + $numero2;
+//restar
+echo $numero1 - $numero2;
 
+//multiplicar
+echo $numero1 * $numero2;
+//divicion
+echo $numero1 / $numero2;
+//multiplicar cierta cantidad de veces
+echo 2**3;
 
-
+echo "<br>";
 include 'includes/footer.php';
--- 05-comparacion.php
+++ 05-comparacion.php
@@ -1,7 +1,30 @@
 <?php include 'includes/header.php';
 
+$nuemro1=20;
+$nuemro2=20;
+$nuemro3=20;
+$nuemro4="30";
 
+var_dump($nuemro1 > $nuemro2);
+echo "<br/>";
 
+var_dump($nuemro1 < $nuemro2);
+echo "<br/>";
 
+var_dump($nuemro1 >= $nuemro2);
+echo "<br/>";
 
+var_dump($nuemro1 <= $nuemro2);
+echo "<br/>";
+
+var_dump($nuemro2 == $nuemro3);
+echo "<br/>";
+
+var_dump($nuemro2 === $nuemro4);
+echo "<br/>";
+
+// -1 si izquierda  es menor , o si es igual 1 a la izquierda es mayor
+
+var_dump($nuemro1 <=> $nuemro2);
+echo "<br>";
 include 'includes/footer.php';
