e1d8956f0ec118148a2d9c0bee05425cd54b45c8
oscar cervantes <oscarcr189@outlook.com>
2023-03-27 00:03:37 -0600

    actualizacion 3array

--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,5 +1,25 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'premium'
+    ]
+];
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+// echo $cliente['nombre'];
+// echo $cliente['informacion']['disponible'];
+
+$cliente['codigo'] = 12354984;
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 
 
--- 10-isset-empty.php
+++ 10-isset-empty.php
@@ -1,6 +1,25 @@
 <?php include 'includes/header.php';
 
+$clientes = [];
+$clientes = array();
+$clientes3 = array('Pedro', 'Juan','karen');
 
+//Revisa si un arreglo esta vacio
+var_dump(empty ($clientes));
+var_dump(empty ($clientes3));
+var_dump(empty ($clientes2));
+
+// Isset -revisar si un arreglo esta creado o una propiedad esta definida
+
+echo "<br>";
+var_dump(isset ($clientes4));
+var_dump(isset ($clientes));
+var_dump(isset ($clientes2));
+var_dump(isset ($clientes3));
+
+//permite revisar si unapropiedad de un arreglo asiciado existe
+var_dump(isset ($clientes['nombre']));
+var_dump(isset ($clientes['codigo']));
 
 
 include 'includes/footer.php';
