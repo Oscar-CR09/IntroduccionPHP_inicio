ebd2a76cfe12ea91a3cd14dab84a296f675e3559
oscar cervantes <oscarcr189@outlook.com>
2023-03-30 00:12:35 -0600

    loops actualizacion6

--- 13-loops.php
+++ 13-loops.php
@@ -4,9 +4,33 @@ while($i <10){
     echo$i . '<br>';
 
     $i++; //incemento
-    
+
+    $i=0;
 
 }
 
+do {
+    echo $i . '<br>';
+    $i++; //incemento
+
+} while($i < 10);
+
+/*
+    3 imprimir Fizz
+    5 imprimir buzz
+    3y 5 imprimir fizz Buzz 
+
+*/ 
+
+
+for($i =0;$i<100;$i++) {
+    if($i%15 === 0) {
+        echo $i . '- FIZZ BUZZ <br>';
+    }
+    elseif($i%3=== 0){
+        echo $i . '- FIZZ  <br>';
+    }
+    echo $i . '<br>';
+}
 
 include 'includes/footer.php';
--- 14-foreach.php
+++ 14-foreach.php
@@ -1,6 +1,105 @@
 <?php include 'includes/header.php';
 
+/*
+    3 imprimir Fizz
+    5 imprimir buzz
+    3y 5 imprimir fizz Buzz 
+
+*/ 
+
+for($i = 1; $i<100 ; $i++) {
+    if($i % 3 === 0 && $i % 5 === 0) {
+        echo $i . '- FIZZ BUZZ <br/>';
+    }
+    elseif($i % 3=== 0){
+        echo $i . '- FIZZ  <br/>';
+    }elseif($i % 5 === 0){
+        echo $i . '- BUZZ <br/>';
+
+    }
+
+}
+$cleinters = array('Pedro', 'Juan', 'karen');
+
+foreach($cleinters as $cliente ){
+    echo $cliente . '<br/>';
+
+}
+
+// echo count($cleinters);
+echo sizeof($cleinters);
+
+for($i = 0; $i < count($cleinters) ; $i++){
+    echo $cleinters[$i] . '<br/>';
+
+}
+
+$cliente = [
+    'nombre' => 'oscar',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' =>'premium'
+    ]
+    ];
+
+foreach ($cliente as $key => $valor) {
+    echo $key . "-" .  $valor . '<br/>';
+
+}
 
 
+$productos = [
+    [
+        'nombre' => 'Tablet',
+        'Precio' => 200,
+        'disponible' => true
+    ],
+
+    [
+        'nombre' => 'Televicion',
+        'Precio' => 300,
+        'disponible' => true
+    ],
+
+    [
+        'nombre' => 'Monitor curvo',
+        'Precio' => 400,
+        'disponible' => false   
+
+    ]
+    ];
+
+
+foreach ($productos as $producto){ ?>
+    <li>
+        <p>Producto: <?php echo $producto['nombre']; ?> </p>
+        <p>Precio: <?php echo "$" . $producto['precio']; ?></p>
+        <p> <?php echo($producto['disponible'])?'Disponible' : 'No disponible'; ?></p>
+
+    </li>
+    <?php
+      
+}
+
+// foreach ($productos as $producto){
+//     echo "<pre>";
+//     var_dump ($producto);
+//     echo "</pre>";
+
+// }
+
+// <?php
+// if ($producto['disponible']) {
+//     echo "<p>Disponible</p>";
+// }else{
+//     echo "<p>No Disponible</p>";
+// }
+
+
+/* ?>
+//  echo "<pre>";
+//  var_dump ($producto);
+//  echo "</pre>";
+*/
 
 include 'includes/footer.php';
