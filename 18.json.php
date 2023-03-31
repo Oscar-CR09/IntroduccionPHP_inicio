6f1bd88265f94dcb334e206b4b5225ff20b0f11f
oscar cervantes <oscarcr189@outlook.com>
2023-03-30 23:54:12 -0600

    actualizacion9

--- 18.json.php
+++ 18.json.php
@@ -1,8 +1,33 @@
 <?php include 'includes/header.php';
 
 
+$productos = [
+    [
+        'nombre' => 'Tablet',
+        'Precio' => 200,
+        'disponible' => true
+    ],
 
+    [
+        'nombre' => 'Televicion',
+        'Precio' => 300,
+        'disponible' => true
+    ],
 
+    [
+        'nombre' => 'Monitor curvo',
+        'Precio' => 400,
+        'disponible' => false   
 
+    ]
+    ];
+echo "<pre>";
+var_dump ($producto);
+echo "</pre>";
+$json = json_encode($producto,json_unescape_unicode);
+$json_array = json_encode($jason);
+
+var_dump($json);
+echo "</pre>";
 
 include 'includes/footer.php';
