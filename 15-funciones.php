b719dd9c5cd009c7c32dbbf5b20d1a88d086ce78
oscar cervantes <oscarcr189@outlook.com>
2023-03-30 23:35:30 -0600

    actualizacion 7

--- 15-funciones.php
+++ 15-funciones.php
@@ -1,5 +1,20 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types=1);
+include 'includes/header.php';
 
+// declare(strict_types=1);
+
+function sumar(int $n1=0,int $n2=0) {
+    echo $n1+$n2;
+}
+
+sumar(10,20);
+echo '<br>';
+sumar(10,2);
+echo '<br>';
+sumar(10,5);
+echo '<br>';
+sumar(10,3);
 
 
 
--- 16-types.php
+++ 16-types.php
@@ -1,7 +1,16 @@
 <?php include 'includes/header.php';
 
 
+function usuarioAutenticado(bool $autenticado): string | int {
+    if($autenticado){
+        return "el usuario est autenticado.";
+    }else{
+        return 20;
+    }
 
+}
+$usuario= usuarioAutenticado(false);
+echo $usuario;
 
 
 include 'includes/footer.php';
